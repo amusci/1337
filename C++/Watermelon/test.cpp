b63b083014c35422f9f0524674bffc22c947e23d
Antonio Musciano <antoniomusciano@gmail.com>
2021-08-11 22:28:13 -0400

    test

---
+++ C++/Watermelon/test.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+double cube(double num) {
+
+    double result = num * num * num;
+
+    return result;
+
+}
+
+int main() {
+
+    cout << cube(3);
+    
+
+    return 0;
+}
+
